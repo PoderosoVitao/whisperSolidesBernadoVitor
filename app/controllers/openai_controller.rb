9f8529f7a849951bbc0d57e39eec3618209b9a8a
Victor Braz <victorhbraz2004@gmail.com>
2025-02-28 15:05:20 -0300

    converte .opus para .ogg

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -4,6 +4,15 @@ class OpenaiController < ApplicationController
   skip_before_action :verify_authenticity_token
   def forward
     audio_binario = params["audioName"]
+
+    # re-escreve o conteudo de um arquivo .opus para um novo arquivo .ogg
+    if audio_binario.respond_to?(:original_filename) && File.extname(audio_binario.original_filename) == ".opus"
+      temp_file = Tempfile.new([ "converted", ".ogg" ], binmode: true)
+      temp_file.write(audio_binario.read)
+      temp_file.rewind
+      audio_binario = temp_file
+    end
+
     service_openai = ::OpenaiService.new()
     transcricao = service_openai.transcribe(audio_binario)
 
